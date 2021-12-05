74bb46eb5583335f41bb7a7da006a37c83f86efa
Kamil S <switalski.kamil@gmail.com>
2021-12-05 15:27:15 +0100

    init

---
+++ src/main/java/com/mnk/fastfood/web/start/StartController.java
@@ -0,0 +1,14 @@
+package com.mnk.fastfood.web.start;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StartController {
+	
+	@GetMapping("/")
+	public String start() {
+		return "start";
+	}
+
+}
---
+++ src/test/java/com/mnk/fastfood/FastFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnk.fastfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
